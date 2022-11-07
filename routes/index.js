d892ebd8f3d81b2c01be37c4fd5fe270006e7b54
Denis <denislazo1610@gmail.com>
2022-11-07 12:44:29 -0700

    Second assignment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require("express").Router();
 const { nombre } = require("../controller/index");
 
-routes.get("/", nombre);
+// routes.get("/", nombre);
+
+routes.use("/contacts", require("./contacts"));
 
 module.exports = routes;
